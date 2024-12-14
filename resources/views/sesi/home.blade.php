9c5b531cdefdb5274dadcc2f4147a7905d7ecf4c
Marcel Hans Sasongko <marcelsasongko11@gmail.com>
2024-12-14 15:55:51 +0700

    b

--- resources/views/sesi/home.blade.php
+++ resources/views/sesi/home.blade.php
@@ -19,8 +19,6 @@
                 <h1>Hi, {{ Auth::user()->username }}</h1>
             </div>
         </div>
-
-        
     </nav>
 
     <header class="mt-20 bg-sky-950 py-16">
@@ -28,28 +26,30 @@
             <h1 class="text-4xl font-bold text-blue-600">Discover Your Next Favorite Book</h1>
             <p class="text-white mt-4">Explore a wide range of books and order online with ease.</p>
             <form action="">
-                <input type="text" id="search" name="search" class="w-1/2 mt-2 p-3 border rounded-lg" placeholder="Search for books...">
-                <button type="submit" class="bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600"><i class="bi-search"></i></button>
+                <input type="text" id="search" name="search" class="w-1/2 mt-2 p-3 border rounded-lg"
+                    placeholder="Search for books...">
+                <button type="submit" class="bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600"><i
+                        class="bi-search"></i></button>
             </form>
         </div>
     </header>
 
-    <!-- Menampilkan judul buku pertama dalam tag <h1> -->
-    @if($books->count() > 0)
+    @if ($books->count() > 0)
         <h1 class="text-center text-3xl mt-8">Featured Book: {{ $books->first()->judul }}</h1>
     @else
         <h1 class="text-center text-3xl mt-8">No Books Available</h1>
     @endif
 
-    <div class="container mx-auto mt-10 grid grid-cols-3 gap-8">
+    <div class="container mx-auto mt-10 mb-10 grid grid-cols-4 gap-8">
         @foreach ($books as $book)
-        <div class="bg-white p-4 rounded shadow-lg">
-            <!-- Display the image and book title -->
-            <img src="{{ asset('storage/' . $book->gambar) }}" alt="{{ $book->judul }}" class="w-full h-64 object-cover rounded">
-            <h2 class="mt-4 text-xl font-bold text-center">{{ $book->judul }}</h2>
-        </div>
+            <div class="bg-white p-4 rounded shadow-lg">
+                <!-- Display the image and book title -->
+                <img src="{{ $book->gambar }}" alt="{{ $book->judul }}" class="w-full h-64 object-cover rounded">
+                <h2 class="mt-4 text-xl font-bold text-center">{{ $book->judul }}</h2>
+            </div>
         @endforeach
     </div>
+
 
     <footer class="bg-gray-800 text-white py-6">
         <div class="container mx-auto text-center">
